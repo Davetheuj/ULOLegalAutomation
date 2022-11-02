7b3b7a232f76f12a0c0ab4f066d93e9855370b57
David Underwood-Jett <=>
2022-11-02 14:38:31 -0700

    adding heroku

--- server/server.js
+++ server/server.js
@@ -7,12 +7,15 @@ const usersRouter = require('./routes/users');
 const legalProcessesRouter = require('./routes/legal_processes');
 
 const app = express();
-const port = environment.PORT || 5000;
+const port = process.env.PORT || 5000;
+
+require("dotenv").config();
+
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(environment.ATLAS_URI, {useNewUrlParser: true});
+mongoose.connect(process.env.ATLAS_URI, {useNewUrlParser: true});
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established!");
@@ -20,6 +23,16 @@ connection.once('open', () => {
 
 app.use('/LegalProcesses', legalProcessesRouter);
 app.use('/Users', usersRouter);
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "../build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "../build", "index.html"));
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
